db1474cd8e725397ec75f8715a634f867204cb55
Raquel e Silva <raquelesilva99@gmail.com>
2019-11-10 21:58:52 +0000

    Reset button not working

---
+++ tetris.js
@@ -0,0 +1,11 @@
+const canvas1 = document.querySelector("#c1");
+const ctx1 = canvas1.getContext("2d");
+
+const canvas2 = document.querySelector("#c2");
+const ctx2 = canvas2.getContext("2d");
+
+document.getElementById("reset").addEventListener("click", function resetGame())
+
+function resetGame() {
+    ctx1.clearRect(0, 0, canvas1.width, canvas1.height)
+}
