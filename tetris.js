4ef85827289564bdf139e5a1974a6d125bdbc6d1
Raquel e Silva <raquelesilva99@gmail.com>
2019-11-10 22:09:44 +0000

    reset button done

--- tetris.js
+++ tetris.js
@@ -4,8 +4,6 @@ const ctx1 = canvas1.getContext("2d");
 const canvas2 = document.querySelector("#c2");
 const ctx2 = canvas2.getContext("2d");
 
-document.getElementById("reset").addEventListener("click", function resetGame())
-
-function resetGame() {
+document.getElementById("reset").addEventListener("click", function resetGame(){
     ctx1.clearRect(0, 0, canvas1.width, canvas1.height)
-}
+})
