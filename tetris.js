1053fbdb1df52de8fdbf0102db7fcf303e6dc4c7
Raquel e Silva <raquelesilva99@gmail.com>
2019-11-24 18:00:41 +0000

    Queda dos objetos

--- tetris.js
+++ tetris.js
@@ -10,48 +10,27 @@ document.getElementById("start").addEventListener("click", function resetGame() 
     ctx1.clearRect(0, 0, canvas1.width, canvas1.height)
 })
 
-let peça = function (type) {
-    if (type === "t") {
-        return [
-            [0, 0, 0],
-            [5, 5, 5],
-            [0, 5, 0]
-        ]
-    } else if (type === "l") {
-        return [
-            [0, 5, 0],
-            [0, 5, 0],
-            [0, 5, 0],
-            [0, 5, 5],
-        ]
-    } else if (type === "j") {
-        return [
-            [0, 5, 0],
-            [0, 5, 0],
-            [5, 5, 0],
-        ]
-    } else if (type === "i") {
-        return [
-            [0, 5, 0],
-            [0, 5, 0],
-            [0, 5, 0],
-        ]
-    } else if (type === "o") {
-        return [
-            [5, 5],
-            [5, 5],
-        ]
-    } else if (type === "s") {
-        return [
-            [0, 5, 5],
-            [5, 5, 0],
-            [0, 0, 0],
-        ]
-    } else if (type === "z") {
-        return [
-            [5, 5, 0],
-            [0, 5, 5],
-            [0, 0, 0],
-        ]
-    }
+let b = new Array();
+function creatPiece() {
+    b.push(new piece(x, y, vel, color, r))
 }
+
+function render() {
+    //fade Canvas
+    ctx.fillStyle = "rgba(255,255,255,0.25)"
+    ctx.fillRect(0, 0, W, H);
+
+    //draw
+    b.forEach(function (piece) {
+        piece.draw();
+    });
+
+    b[b.length - 1].update();
+    if (b[b.length - 1].stop) {
+        creatPiece();
+    }
+
+    //new frame
+    window.requestAnimationFrame(render);
+}
+render();  //start animate
