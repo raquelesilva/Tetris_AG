b8b82e8a931bc1e7f5fbb91cb9e1bbac166c4583
fspereira95 <9160127@esmad.ipp.pt>
2019-11-24 18:38:45 +0000

    Fix green box

--- tetris.js
+++ tetris.js
@@ -1,37 +1,52 @@
 const canvas1 = document.querySelector("#c1");
-const ctx = canvas1.getContext("2d");
+const ctx1 = canvas1.getContext("2d");
 const scoreElement = document.getElementById("score");
 
 const canvas2 = document.querySelector("#c2");
 const ctx2 = canvas2.getContext("2d");
 
-context.scale(20, 20);
+var W = canvas1.width;
+var H = canvas1.height;
+
 
 document.getElementById("start").addEventListener("click", function resetGame() {
     ctx1.clearRect(0, 0, canvas1.width, canvas1.height)
 })
 
-let b = new Array();
-function creatPiece() {
-    b.push(new piece(x, y, vel, color, r))
+const row = 20;
+const col = column = 10;
+const sq = squareSize = 50;
+const vacant = "black"; // color of an empty square
+
+// draw a square
+function drawSquare(x,y,color){
+    ctx1.fillStyle = color;
+    ctx1.fillRect(x*sq,y*sq,sq,sq);
+
+    ctx1.strokeStyle = "white";
+    ctx1.strokeRect(x*sq,y*sq,sq,sq);
 }
 
-function render() {
-    //fade Canvas
-    ctx.fillStyle = "rgba(255,255,255,0.25)"
-    ctx.fillRect(0, 0, W, H);
+// create the board
 
-    //draw
-    b.forEach(function (piece) {
-        piece.draw();
-    });
-
-    b[b.length - 1].update();
-    if (b[b.length - 1].stop) {
-        creatPiece();
+let board = [];
+for( r = 0; r <row; r++){
+    board[r] = [];
+    for(c = 0; c < col; c++){
+        board[r][c] = vacant;
     }
-
-    //new frame
-    window.requestAnimationFrame(render);
 }
-render();  //start animate
+
+// draw the board
+function drawBoard(){
+    for( r = 0; r <row; r++){
+        for(c = 0; c < col; c++){
+            drawSquare(c,r,board[r][c]);
+        }
+    }
+}
+
+drawBoard();
+
+
+
