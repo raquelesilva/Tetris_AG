129d6698bce12154af685e3eac3e029fc9f591ef
fspereira95 <9160127@esmad.ipp.pt>
2019-11-24 18:12:28 +0000

    Merge branch 'master' of https://github.com/raquelesilva/Tetris_AG

--- tetris.js
+++ tetris.js
@@ -1,9 +1,41 @@
 const canvas1 = document.querySelector("#c1");
-const ctx1 = canvas1.getContext("2d");
+const ctx = canvas1.getContext("2d");
+const scoreElement = document.getElementById("score");
 
 const canvas2 = document.querySelector("#c2");
 const ctx2 = canvas2.getContext("2d");
 
-document.getElementById("reset").addEventListener("click", function resetGame(){
+context.scale(20, 20);
+
+document.getElementById("start").addEventListener("click", function resetGame() {
     ctx1.clearRect(0, 0, canvas1.width, canvas1.height)
 })
+
+<<<<<<< HEAD
+
+=======
+let b = new Array();
+function creatPiece() {
+    b.push(new piece(x, y, vel, color, r))
+}
+
+function render() {
+    //fade Canvas
+    ctx.fillStyle = "rgba(255,255,255,0.25)"
+    ctx.fillRect(0, 0, W, H);
+
+    //draw
+    b.forEach(function (piece) {
+        piece.draw();
+    });
+
+    b[b.length - 1].update();
+    if (b[b.length - 1].stop) {
+        creatPiece();
+    }
+
+    //new frame
+    window.requestAnimationFrame(render);
+}
+render();  //start animate
+>>>>>>> 1053fbdb1df52de8fdbf0102db7fcf303e6dc4c7
