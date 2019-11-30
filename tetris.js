c3eb4a7c53a0eb4776751b6f787c38c9ffa64ecd
fspereira95 <9160127@esmad.ipp.pt>
2019-11-30 18:39:04 +0000

    UPDATED GAME(MOVEMENT , RANDOM CREATE PIECE)

--- tetris.js
+++ tetris.js
@@ -17,8 +17,17 @@ const row = 20;
 const col = column = 10;
 const sq = squareSize = 50;
 const vacant = "black"; // color of an empty square
+let x = 2;
+let y = -1;
+let frameCounter = 0;
+const T = 1;
+let rnd;
 
-// draw a square
+window.addEventListener('keydown', ArrowPressed);
+window.addEventListener('keyup', ArrowReleased);
+
+
+// DESENHAR UM QUADRADO
 function drawSquare(x, y, color) {
     ctx1.fillStyle = color;
     ctx1.fillRect(x * sq, y * sq, sq, sq);
@@ -27,7 +36,7 @@ function drawSquare(x, y, color) {
     ctx1.strokeRect(x * sq, y * sq, sq, sq);
 }
 
-// create the board
+// CRIAR O CAMPO
 
 let board = [];
 for (r = 0; r < row; r++) {
@@ -37,7 +46,8 @@ for (r = 0; r < row; r++) {
     }
 }
 
-// draw the board
+// DESENHAR O CAMPO
+
 function drawBoard() {
     for (r = 0; r < row; r++) {
         for (c = 0; c < col; c++) {
@@ -47,3 +57,99 @@ function drawBoard() {
 }
 
 drawBoard();
+
+//ANIMATION
+
+function render() {
+
+    drawBoard();
+
+    if(y == -1 || y == 19){
+        
+        rnd = randomPiece()
+        
+
+        //FOR THE T PIECE
+        if (y == 19 && rnd == 1) {
+            y = -1;
+          
+            x = Math.floor(Math.random() * 8) + 1;
+            console.log(x);
+        }
+    }
+    drawPiece(x, y, "blue" , rnd);
+    
+
+
+    if (frameCounter == 10) {
+
+        dropPiece(y);
+        frameCounter = 0;
+    }
+
+    frameCounter++
+}
+render();
+
+function drawPiece(x, y, color, rnd) {
+
+       
+
+    if (rnd == T) {
+
+
+        ctx.fillStyle = color
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(x * sq, y * sq, sq, sq);
+        ctx.fillRect(x * sq, y * sq, sq, sq);
+        ctx.strokeRect((x - 1) * sq, (y - 1) * sq, sq, sq);
+        ctx.fillRect((x - 1) * sq, (y - 1) * sq, sq, sq);
+        ctx.strokeRect(x * sq, (y - 1) * sq, sq, sq);
+        ctx.fillRect(x * sq, (y - 1) * sq, sq, sq);
+        ctx.strokeRect((x + 1) * sq, (y - 1) * sq, sq, sq);
+        ctx.fillRect((x + 1) * sq, (y - 1) * sq, sq, sq);
+    }
+}
+
+//MOVE PIECE TO THE RIGHT AND LEFT
+
+
+function ArrowPressed(e) {
+    if (e.key == 'ArrowRight' && y < 19 && x < 8) {
+        x++;
+    }
+    if (e.key == 'ArrowLeft' && y < 19 && x > 1) {
+        x--;
+    }
+}
+
+function ArrowReleased(e) {
+    if (e.key == 'ArrowRight') {
+
+    }
+    if (e.key == 'ArrowLeft') {
+
+    }
+}
+
+
+//FUNÇÃO PARA A PEÇA CAIR
+
+function dropPiece() {
+    if (y < 19) {
+        y++
+    }
+
+}
+
+window.onload = function () {
+    timer = window.setInterval(render, 100)
+}
+
+//FUNÇÃO PARA A CAIR UMA PEÇA RANDOM
+
+function randomPiece() {
+
+    return Math.floor(Math.random() * 0) + 1;
+
+}
