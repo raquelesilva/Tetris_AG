44ca9bc18dceac90a9241c2373514a7e053dbd2b
Raquel e Silva <raquelesilva99@gmail.com>
2019-12-13 14:39:21 +0000

    peças

--- tetris.js
+++ tetris.js
@@ -5,35 +5,233 @@ const scoreElement = document.getElementById("score");
 const canvas2 = document.querySelector("#c2");
 const ctx2 = canvas2.getContext("2d");
 
-var W = canvas1.width;
-var H = canvas1.height;
+const W = canvas1.width;
+const H = canvas1.height;
 
 
 document.getElementById("start").addEventListener("click", function resetGame() {
-    ctx1.clearRect(0, 0, canvas1.width, canvas1.height)
+    ctx1.clearRect(0, 0, W, H)
 })
 
-const row = 20;
-const col = column = 10;
-const sq = squareSize = 35;
+const row = 22;
+const col = column = 12;
+const sq = squareSize = 50;
 const vacant = "black"; // color of an empty square
 let x = 2;
-let y = -1;
+let y = 1;
+
+let x1, x2, x3, x4 = 0
+let y1, y2, y3, y4 = 0
+
 let frameCounter = 0;
-const T = 1;
+const T = 1; // vai ser o tipo
 let rotCounter = 1;
 let rnd;
+let stop = false
+let type = 0 /* Math.floor(Math.random() * 6) */
 
 window.addEventListener('keydown', ArrowPressed);
 window.addEventListener('keyup', ArrowReleased);
 
+class Piece {   	//construtor
+    constructor() {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.x3 = x3;
+        this.x4 = x4;
+        this.y1 = y1;
+        this.y2 = y2;
+        this.y3 = y3;
+        this.y4 = y4;
+        this.type = type;
+    }
+
+    draw() {
+        if (this.type == 0) { // T
+            ctx1.fillStyle = "pink";
+            ctx1.strokeStyle = "black";
+            ctx1.strokeRect(x1, y1, sq, sq);
+            ctx1.fillRect(x1, y1, sq, sq);
+            ctx1.strokeRect(x2, y2, sq, sq);
+            ctx1.fillRect(x2, y2, sq, sq);
+            ctx1.strokeRect(x3, y3, sq, sq);
+            ctx1.fillRect(x3, y3, sq, sq);
+            ctx1.strokeRect(x4, y4, sq, sq);
+            ctx1.fillRect(x4, y4, sq, sq);
+        }
+        else if (this.type == 1) { //I
+            ctx1.fillStyle = "cyan";
+            ctx1.strokeStyle = "black";
+            ctx1.strokeRect(x1, y1, sq, sq);
+            ctx1.fillRect(x1, y1, sq, sq);
+            ctx1.strokeRect(x2, y2, sq, sq);
+            ctx1.fillRect(x2, y2, sq, sq);
+            ctx1.strokeRect(x3, y3, sq, sq);
+            ctx1.fillRect(x3, y3, sq, sq);
+            ctx1.strokeRect(x4, y4, sq, sq);
+            ctx1.fillRect(x4, y4, sq, sq);
+        }
+        else if (this.type == 2) { // 0
+            ctx1.fillStyle = "blue";
+            ctx1.strokeStyle = "black";
+            ctx1.strokeRect(x1, y1, sq, sq);
+            ctx1.fillRect(x1, y1, sq, sq);
+            ctx1.strokeRect(x2, y2, sq, sq);
+            ctx1.fillRect(x2, y2, sq, sq);
+            ctx1.strokeRect(x3, y3, sq, sq);
+            ctx1.fillRect(x3, y3, sq, sq);
+            ctx1.strokeRect(x4, y4, sq, sq);
+            ctx1.fillRect(x4, y4, sq, sq);
+        }
+        else if (this.type == 3) { // L
+            ctx1.fillStyle = "yellow";
+            ctx1.strokeStyle = "black";
+            ctx1.strokeRect(x1, y1, sq, sq);
+            ctx1.fillRect(x1, y1, sq, sq);
+            ctx1.strokeRect(x2, y2, sq, sq);
+            ctx1.fillRect(x2 , y2, sq, sq);
+            ctx1.strokeRect(x3, y3, sq, sq);
+            ctx1.fillRect(x3, y3, sq, sq);
+            ctx1.strokeRect(x4, y4 , sq, sq);
+            ctx1.fillRect(x4, y4, sq, sq);
+        }
+        else if (this.type == 4) { // Z
+            ctx1.fillStyle = "red";
+            ctx1.strokeStyle = "black";
+            ctx1.strokeRect(x1, y1, sq, sq);
+            ctx1.fillRect(x1, y1, sq, sq);
+            ctx1.strokeRect(x2, y2, sq, sq);
+            ctx1.fillRect(x2, y2, sq, sq);
+            ctx1.strokeRect(x3, y3, sq, sq);
+            ctx1.fillRect(x3, y3, sq, sq);
+            ctx1.strokeRect(x4, y4, sq, sq);
+            ctx1.fillRect(x4, y4, sq, sq);
+        }
+        else if (this.type == 5) { // S
+            ctx1.fillStyle = "green";
+            ctx1.strokeStyle = "black";
+            ctx1.strokeRect(x1, y1, sq, sq);
+            ctx1.fillRect(x1, y1, sq, sq);
+            ctx1.strokeRect(x2, y2, sq, sq);
+            ctx1.fillRect(x2, y2, sq, sq);
+            ctx1.strokeRect(x3 , y3, sq, sq);
+            ctx1.fillRect(x3 , y3, sq, sq);
+            ctx1.strokeRect(x4 , y4, sq, sq);
+            ctx1.fillRect(x4, y4, sq, sq);
+        }
+        else if (this.type == 6) { // J
+            ctx1.fillStyle = "orange";
+            ctx1.strokeStyle = "black";
+            ctx1.strokeRect(x1, y1, sq, sq);
+            ctx1.fillRect(x1, y1, sq, sq);
+            ctx1.strokeRect(x2, y2, sq, sq);
+            ctx1.fillRect(x2, y2, sq, sq);
+            ctx1.strokeRect(x3, y3, sq, sq);
+            ctx1.fillRect(x3, y3, sq, sq);
+            ctx1.strokeRect(x4, y4, sq, sq);
+            ctx1.fillRect(x4, y4, sq, sq);
+        }
+    }
+
+    update() {
+        if (this.y1 == H - sq || this.y2 == H - sq || this.y3 == H - sq || this.y4 == H - sq) {
+            console.log("PAREI");
+            this.stop = true;
+            createPiece()
+        } else {
+            console.log("COMECEI");
+            y1 += sq;
+            y2 += sq;
+            y3 += sq;
+            y4 += sq;
+        }
+    }
+
+    createPiece() {
+        if (this.type == 0) /* T */ {
+            x1 = 4 * sq;
+            x2 = 5 * sq;
+            x3 = 6 * sq;
+            x4 = 5 * sq;
+            y1 = 0;
+            y2 = 0;
+            y3 = 0;
+            y4 = sq;
+
+            console.log(this.x1, "/", this.x2, "/", this.x3, "/" ,this.x4)
+        }
+        else if (this.type == 1) /* I */ {
+            x1 = 3 * sq;
+            x2 = 4 * sq;
+            x3 = 5 * sq;
+            x4 = 6 * sq;
+            y1 = 0;
+            y2 = 0;
+            y3 = 0;
+            y4 = 0;
+        }
+        else if (this.type == 2) /* O */ {
+            x1 = 4 * sq;
+            x2 = 5 * sq;
+            x3 = 4 * sq;
+            x4 = 5 * sq;
+            y1 = 0;
+            y2 = 0;
+            y3 = sq;
+            y4 = sq;
+        }
+        else if (this.type == 3) /* L */ {
+            x1 = 4 * sq;
+            x2 = 4 * sq;
+            x3 = 4 * sq;
+            x4 = 5 * sq;
+            y1 = 0;
+            y2 = sq;
+            y3 = 2 * sq;
+            y4 = 2 * sq;
+        }
+        else if (this.type == 4) /* Z */ {
+            x1 = 4 * sq;
+            x2 = 5 * sq;
+            x3 = 5 * sq;
+            x4 = 6 * sq;
+            y1 = 0;
+            y2 = 0;
+            y3 = sq;
+            y4 = sq;
+        }
+        else if (this.type == 5) /* S */ {
+            x1 = 5 * sq;
+            x2 = 4 * sq;
+            x3 = 4 * sq;
+            x4 = 3 * sq;
+            y1 = 0;
+            y2 = 0;
+            y3 = sq;
+            y4 = sq;
+        }
+        else if (this.type == 6) /* J */ {
+            x1 = 4 * sq;
+            x2 = 4 * sq;
+            x3 = 4 * sq;
+            x4 = 3 * sq;
+            y1 = 0;
+            y2 = sq;
+            y3 = 2 * sq;
+            y4 = 2 * sq;
+        }
+    }
+}
+
+let pieces = new Array();
+pieces.push(new Piece());
 
 // DESENHAR UM QUADRADO
 function drawSquare(x, y, color) {
     ctx1.fillStyle = color;
     ctx1.fillRect(x * sq, y * sq, sq, sq);
 
-    ctx1.strokeStyle = "black";
+    ctx1.strokeStyle = "white";
     ctx1.strokeRect(x * sq, y * sq, sq, sq);
 }
 
@@ -48,7 +246,6 @@ for (r = 0; r < row; r++) {
 }
 
 // DESENHAR O CAMPO
-
 function drawBoard() {
     for (r = 0; r < row; r++) {
         for (c = 0; c < col; c++) {
@@ -60,61 +257,58 @@ function drawBoard() {
 drawBoard();
 
 //ANIMATION
-
 function render() {
-
+    ctx1.clearRect(0, 0, W, H)
     drawBoard();
 
-    if(y == -1 || y == 19){
-        
+    pieces.forEach(function (piece) {
+        piece.draw();
+        piece.update();
+    });
+
+    if (!stop) {
+        console.log("hello")
+        console.log(type)
+        pieces[pieces.length - 1].createPiece()
+    } else {
+        console.log(type)
+    }
+
+    /* if (y == 1 || y == 20) {
         rnd = randomPiece()
-        
-
         //FOR THE T PIECE
-        if (y == 19 && rnd == 1) {
-            y = -1;
-          
-            x = Math.floor(Math.random() * 8) + 1;
-            console.log(x);
+        if (y == 20 && rnd == 1) {
+
         }
-    }
-    drawPiece(x, y, "blue" , rnd);
-    
+    } */
+    /* drawPiece(x, y, "blue", rnd); */
 
-
-    if (frameCounter == 10) {
-
-        dropPiece(y);
-        frameCounter = 0;
-    }
-
-    frameCounter++
+        if (frameCounter == 10) {
+            /* dropPiece(y); */
+            frameCounter = 0;
+        }
+        frameCounter++
 }
-render();
 
-function drawPiece(x, y, color, rnd) {
-
-       
-
+//desnhea a peca T: rnd -> rotação
+/* function drawPiece(x, y, color, rnd) {
     if (rnd == T) {
-
-
         ctx1.fillStyle = color
         ctx1.strokeStyle = "white";
         ctx1.strokeRect(x * sq, y * sq, sq, sq);
         ctx1.fillRect(x * sq, y * sq, sq, sq);
+
         ctx1.strokeRect((x - 1) * sq, (y - 1) * sq, sq, sq);
         ctx1.fillRect((x - 1) * sq, (y - 1) * sq, sq, sq);
+
         ctx1.strokeRect(x * sq, (y - 1) * sq, sq, sq);
         ctx1.fillRect(x * sq, (y - 1) * sq, sq, sq);
+
         ctx1.strokeRect((x + 1) * sq, (y - 1) * sq, sq, sq);
         ctx1.fillRect((x + 1) * sq, (y - 1) * sq, sq, sq);
     }
 
     if (rnd == 2) {
-        
-        
-
         ctx1.fillStyle = color
         ctx1.strokeStyle = "white";
         ctx1.strokeRect(x * sq, y * sq, sq, sq);
@@ -128,12 +322,9 @@ function drawPiece(x, y, color, rnd) {
     }
 
     if (rnd == 3) {
-        
-        
-
         ctx1.fillStyle = color
         ctx1.strokeStyle = "white";
-        ctx1.strokeRect(x * sq, (y  - 1) * sq, sq, sq);
+        ctx1.strokeRect(x * sq, (y - 1) * sq, sq, sq);
         ctx1.fillRect((x) * sq, (y - 1) * sq, sq, sq);
         ctx1.strokeRect((x - 1) * sq, (y - 1) * sq, sq, sq);
         ctx1.fillRect((x - 1) * sq, (y - 1) * sq, sq, sq);
@@ -144,12 +335,9 @@ function drawPiece(x, y, color, rnd) {
     }
 
     if (rnd == 4) {
-        
-        
-
         ctx1.fillStyle = color
         ctx1.strokeStyle = "white";
-        ctx1.strokeRect(x * sq, (y  - 1) * sq, sq, sq);
+        ctx1.strokeRect(x * sq, (y - 1) * sq, sq, sq);
         ctx1.fillRect((x) * sq, (y - 1) * sq, sq, sq);
         ctx1.strokeRect((x) * sq, (y) * sq, sq, sq);
         ctx1.fillRect((x) * sq, (y) * sq, sq, sq);
@@ -158,27 +346,44 @@ function drawPiece(x, y, color, rnd) {
         ctx1.strokeRect((x) * sq, (y - 2) * sq, sq, sq);
         ctx1.fillRect((x) * sq, (y - 2) * sq, sq, sq);
     }
-}
+} */
+
 //MOVE PIECE TO THE RIGHT AND LEFT
-
-
 function ArrowPressed(e) {
-    if (e.key == 'ArrowRight' && y < 19 && x < 8) {
-        x++;
+    if (e.key == 'ArrowRight' && (this.x1 >= 0 || this.x2 >= 0 || this.x3 >= 0 || this.x4 >= 0)) {
+        pieces[pieces.length - 1].x1 += sq;
+        pieces[pieces.length - 1].x2 += sq;
+        pieces[pieces.length - 1].x3 += sq;
+        pieces[pieces.length - 1].x4 += sq;
+        console.log("gfds")
     }
-    if (e.key == 'ArrowLeft' && y < 19 && x > 1) {
-        x--;
+
+    if (e.key == 'ArrowLeft' && (this.x1 <= H - sq || this.x2 <= H - sq || this.x3 <= H - sq || this.x4 >= H - sq)) {
+        pieces[pieces.length - 1].x1 -= sq;
+        pieces[pieces.length - 1].x2 -= sq;
+        pieces[pieces.length - 1].x3 -= sq;
+        pieces[pieces.length - 1].x4 -= sq;
+        console.log("mexi")
     }
-    if (e.key == 'ArrowUp') {
+
+    /*     if (e.key == 'ArrowUp') {
     
-        if (rotCounter == 4) {
-            
-            rotCounter = 0;
-        }
-        rotCounter++;
-        rnd = rotCounter;
-        console.log("rnd" + " " + rnd)
-        console.log("rotCounter" + " " + rotCounter)
+            if (rotCounter == 4) {
+    
+                rotCounter = 0;
+            }
+            rotCounter++;
+            rnd = rotCounter;
+            console.log("rnd" + " " + rnd)
+            console.log("rotCounter" + " " + rotCounter)
+        } */
+
+    if (e.key == 'ArrowDown') {
+        y1 += sq;
+        y2 += sq;
+        y3 += sq;
+        y4 += sq;
+        console.log("desci")
     }
 }
 
@@ -190,28 +395,25 @@ function ArrowReleased(e) {
 
     }
     if (e.key == 'ArrowUp') {
-        
+
+    }
+    if (e.key == 'ArrowDown') {
+
     }
 }
-
 
 //FUNÇÃO PARA A PEÇA CAIR
-
-function dropPiece() {
-    if (y < 19) {
+/* function dropPiece(e) {
+    if (y < 20) {
         y++
     }
-
-}
+} */
 
 window.onload = function () {
     timer = window.setInterval(render, 100)
 }
 
 //FUNÇÃO PARA A CAIR UMA PEÇA RANDOM
-
-function randomPiece() {
-
+/* function randomPiece() {
     return Math.floor(Math.random() * 0) + 1;
-
-}
+} */
